1da110eec8d30de17f99c7ea47294e5db07cad27
nickarad <nickarad94@gmail.com>
2019-05-04 22:27:06 +0300

    Create Dataset

---
+++ dataset.py
@@ -0,0 +1,66 @@
+# import tensorflow as tf 
+import pandas as pd
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+import labels as lb
+# from tensorflow import keras
+import matplotlib.pyplot as plt 
+  
+def load_data():
+    dir = 'afpdbCSV/'
+    records = [f for f in listdir(dir) if isfile(join(dir, f)) if(f.find('.csv') != -1)]
+    records.sort() 
+    records = records[0:200]
+    print(records)
+    y = lb.get_labels()
+    x = np.zeros((200,1280,2))
+    j = 0
+    for r in records:
+        ecg = pd.read_csv(dir + r, index_col=0)
+        ecg = ecg.values.tolist()
+        i = 0
+        for t in ecg:
+            # print(i,t[1])
+            x[j][i][0] = t[0]
+            x[j][i][1] = t[1]
+            # print(i,x[j][i][0],x[j][i][1])
+            i = i+1
+
+        j = j + 1
+
+
+    return y,x
+
+
+y,x = load_data()
+np.save('x_data.npy', x)
+np.save('y_data.npy', y)
+print(x[199])
+print(y[199])
+
+# dir = 'afpdbCSV/'
+# records = [f for f in listdir(dir) if isfile(join(dir, f)) if(f.find('.csv') != -1)]
+# records.sort() 
+# # r = records[0:2]
+# r = records[0]
+# print(r)
+# ecg = pd.read_csv(dir + r, index_col=0)
+# ecg = ecg.values.tolist()
+
+
+# x = np.zeros((1,1280,2))
+# print(x)
+# print(x.shape)
+# i = 0
+# for t in ecg:
+#     # print(i,t[1])
+#     x[0][i][0] = t[0]
+#     x[0][i][1] = t[1]
+#     print(i,x[0][i][0],x[0][i][1])
+#     i = i+1
+# # print(ecg0)
+# # print(ecg1)
+
+   
+
