533a5f05f99e892e2dd47de768da5a50c580dbc0
nickarad <nickarad94@gmail.com>
2019-05-05 11:31:44 +0300

    train &evaluate model

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from tensorflow import keras
 import numpy as np
 from os import listdir
 from os.path import isfile, join
+import createmodel as crm
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -16,7 +17,7 @@ warnings.filterwarnings("ignore")
 # =========================================== load Dataset ===========================================================
 x = np.load('x_data.npy')
 y = np.load('y_data.npy')
-# print(x)
+print(x.shape)
 # print(y)
 # =========================================== Create dataset ===========================================================
 x_train = x[0:80]
@@ -27,5 +28,25 @@ x_test = x[80:100]
 x_test = np.append(x_test,x[180:200])
 y_test = y[80:100]
 y_test = np.append(y_test,y[180:200])
-# print(y_train)
+x_train = x_train.reshape(-1, 1280,2)
+x_test = x_test.reshape(-1, 1280,2)
+print(x_train.shape)
+print(y_train)
 # print(y_test)
+
+# =========================================== Create Model ===========================================================
+model = crm.create_model()
+model.fit(x_train, y_train, epochs=3)
+# -- model accurancy
+val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
+print(val_loss)  # model's loss (error)
+print(val_acc)  # model's accuracy
+
+# =========================================== Make predictions ===========================================================
+predictions = model.predict(x_test)
+test = 2
+print("prediction:", np.argmax(predictions[test]))
+print("real value:", y_test[test])
+
+# =========================================== Save Model ===========================================================
+model.save('my_model.h5')
