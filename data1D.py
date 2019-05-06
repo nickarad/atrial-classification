5dc631e39e18be65f60a967eafb1a5d09058ac9f
nickarad <nickarad94@gmail.com>
2019-05-06 16:12:13 +0300

    Add .npy files

--- data1D.py
+++ data1D.py
@@ -64,11 +64,16 @@ def load_data():
     return x_train,y_train,x_test,y_test
 
 
-x_train,y_train,x_test,y_test = load_data()
-print(x_train.shape)
-print(y_train.shape)
-print(x_test.shape)
-print(y_test.shape)
+# x_train,y_train,x_test,y_test = load_data()
+# print(x_train.shape)
+# print(y_train.shape)
+# print(x_test.shape)
+# print(y_test.shape)
+
+# np.save('x_train.npy', x_train)
+# np.save('y_train.npy', y_train)
+# np.save('x_test.npy', x_test)
+# np.save('y_test.npy', y_test)
 
 
 
