c92b077eeebc033ab182c356798487748f046b61
nickarad <nickarad94@gmail.com>
2019-05-06 17:23:24 +0300

    80% accurancy

--- trainDense.py
+++ trainDense.py
@@ -17,7 +17,7 @@ x_test = tf.keras.utils.normalize(x_test, axis = 1)
 
 # ************************************* Create Model ***************************************************
 model = dn.create_model()
-model.fit(x_train, y_train, epochs=15)
+model.fit(x_train, y_train, epochs=9)
 # -- model accurancy
 val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
 print(val_loss)  # model's loss (error)
@@ -26,7 +26,7 @@ print(val_acc)  # model's accuracy
 
 # ************************************* Make predictions ***************************************************
 predictions = model.predict(x_test)
-test = 2
+test = 0
 print("prediction:", np.argmax(predictions[test]))
 print("real value:", y_test[test])
 # ======================================================================================================
