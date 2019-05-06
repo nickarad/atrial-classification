b5e7845dc29368c293ad77e29dd0d1d95144378f
nickarad <nickarad94@gmail.com>
2019-05-06 22:45:11 +0300

    Plot Validation Loss & Acc

--- trainDense.py
+++ trainDense.py
@@ -5,7 +5,6 @@ import dense as dn
 from tensorflow import keras
 import data1D as data
 
-
 # ************************************* Prepare Dataset ************************************************
 x_train = np.load('x_train.npy')
 y_train = np.load('y_train.npy')
@@ -17,18 +16,51 @@ x_test = tf.keras.utils.normalize(x_test, axis = 1)
 
 # ************************************* Create Model ***************************************************
 model = dn.create_model()
-model.fit(x_train, y_train, epochs=9)
+history = model.fit(x_train, y_train, epochs=8,validation_data=(x_test, y_test))
 # -- model accurancy
-val_loss, val_acc = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
-print(val_loss)  # model's loss (error)
-print(val_acc)  # model's accuracy
+val_loss1, val_acc1 = model.evaluate(x_test, y_test)  # evaluate the out of sample data with model
+print(val_loss1)  # model's loss (error)
+print(val_acc1)  # model's accuracy
 # ======================================================================================================
-
+# Plot the model Accuracy graph (Ideally, it should be Logarithmic shape)
+plt.plot(history.history['acc'],'r',linewidth=2.0, label='Training Accuracy')
+plt.plot(history.history['val_acc'],'b',linewidth=2.0, label='Testing Accuracy')
+plt.legend(fontsize=18)
+plt.xlabel('Epochs ', fontsize=16)
+plt.ylabel('Accuracy', fontsize=16)
+plt.title('Accuracy Curves', fontsize=16)
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.show()
+# Plot the model Loss graph (Ideally it should be Exponentially decreasing shape)
+plt.plot(history.history['loss'], 'g', linewidth=2.0, label='Training Loss')
+plt.plot(history.history['val_loss'], 'y', linewidth=2.0, label='Testing Loss')
+plt.legend(fontsize=18)
+plt.xlabel('Epochs ', fontsize=16)
+plt.ylabel('Loss', fontsize=16)
+plt.title('Loss Curves', fontsize=16)
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.show()
 # ************************************* Make predictions ***************************************************
 predictions = model.predict(x_test)
-test = 0
+test = 19
 print("prediction:", np.argmax(predictions[test]))
 print("real value:", y_test[test])
+# Plot prediction
+fs = 128
+signal = x_test[test]
+Time=np.linspace(0, len(signal)/fs, num=len(signal))
+
+plt.title('Record 13' )
+plt.plot(Time,signal,'-', lw=1.6)
+# plt.grid(True,which='both', color='0.65', linestyle='-')
+plt.grid(b=True, which='major', color='#666666', linestyle='-')
+plt.minorticks_on()
+plt.grid(b=True, which='minor', color='#999999', linestyle='-', alpha=0.2)
+plt.show()
 # ======================================================================================================
 
 # ************************************* Save Model ***************************************************
