4f0912218fe6a203862dcb6c6eb4ba83f5c917ba
nickarad <nickarad94@gmail.com>
2019-05-17 15:16:42 +0300

    Update .gitignore

--- createmodel.py
+++ createmodel.py
@@ -1,50 +1,51 @@
 import tensorflow as tf
 
 def create_model(lr):
+    dp = 0.5 # define droppout parametre
     model = tf.keras.models.Sequential([
         tf.keras.layers.Dense(1024,kernel_initializer='normal', activation=tf.nn.relu, input_shape=(1280,)),
         # # tf.keras.layers.Dense(4096,kernel_initializer='normal', activation=tf.nn.relu),
-        # tf.keras.layers.Dropout(0.5),
+        # tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(1024,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(2048,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
 
         # tf.keras.layers.Dense(1024,kernel_initializer='normal', activation=tf.nn.relu),
         # # tf.keras.layers.Dense(1024,kernel_initializer='normal', activation=tf.nn.relu),
-        # tf.keras.layers.Dropout(0.5),
+        # tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(512,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(512,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(256,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(256,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(128,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(128,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(64,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(64,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(32,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(64,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(16,kernel_initializer='normal', activation=tf.nn.relu),
         # tf.keras.layers.Dense(64,kernel_initializer='normal', activation=tf.nn.relu),
-        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dropout(dp),
         
         # tf.keras.layers.Dense(8,kernel_initializer='normal', activation=tf.nn.relu),
         # # tf.keras.layers.Dense(64,kernel_initializer='normal', activation=tf.nn.relu),
-        # tf.keras.layers.Dropout(0.5),
+        # tf.keras.layers.Dropout(dp),
 
         # tf.keras.layers.Dense(4,kernel_initializer='normal', activation=tf.nn.relu),
         # # tf.keras.layers.Dense(64,kernel_initializer='normal', activation=tf.nn.relu),
-        # tf.keras.layers.Dropout(0.5),
+        # tf.keras.layers.Dropout(dp),
 
         tf.keras.layers.Dense(2,kernel_initializer='normal', activation=tf.nn.softmax)
     ])
