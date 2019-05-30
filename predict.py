99dc94c03cffaf981f4bfd4967d8d81fe3467e0b
nickarad <nickarad94@gmail.com>
2019-05-31 02:01:17 +0300

    Fix predict.py

--- predict.py
+++ predict.py
@@ -3,8 +3,13 @@ from tensorflow import keras
 import ecg_plot as pl
 import numpy as np
 # ================== Load weights from checkpoint and re-evaluate ===========================
-x_test = np.load('x_test.npy')
-y_test = np.load('y_test.npy')
+x_data = np.load('x_data.npy')
+y_data = np.load('y_data.npy')
+train = 0.7
+size = 200
+x_test = x_data[int(train * size):size]
+y_test = y_data[int(train * size):size]
+print(x_data.shape)
 # x_train = tf.keras.utils.normalize(x_train, axis = 1)
 x_test = tf.keras.utils.normalize(x_test, axis = 1)
 new_model = keras.models.load_model('best.h5')
