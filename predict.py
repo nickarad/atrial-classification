17b2c844be6f51b3be70437b0484ed03b62b8b8d
nickarad <nickarad94@gmail.com>
2019-05-31 11:09:21 +0300

    Add Metrics to predict.py

--- predict.py
+++ predict.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras
 import ecg_plot as pl
 import numpy as np
+from sklearn.metrics import precision_score, recall_score
 # ================== Load weights from checkpoint and re-evaluate ===========================
 x_data = np.load('x_data.npy')
 y_data = np.load('y_data.npy')
@@ -17,19 +18,38 @@ new_model.summary()
 loss, acc = new_model.evaluate(x_test, y_test)
 print("Restored model, accuracy {:5.2f}%".format(100*acc))
 
-# ===========================================================================================
-
 # ================================ Make predictions==========================================
 predictions = new_model.predict(x_test)
 
-for x in range(0,20):
-    num = x
-    # num = 18
-    print(num)
-    print("prediction:", np.argmax(predictions[num]))
-    print("real value:", y_test[num])
-    pl.ecg_plot(x_test[num])
+# for x in range(0,20):
+#     num = x
+#     # num = 18
+#     print(num)
+#     print("prediction:", np.argmax(predictions[num]))
+#     print("real value:", y_test[num])
+#     pl.ecg_plot(x_test[num])
 
+# ************************************* Metrics ***************************************************
+y_pred = np.array([])
+for x in range(0,len(predictions)):
+	y_pred = np.append(y_pred,np.argmax(predictions[x]))
+	
+y_pred = y_pred.astype(int)
 
-# ============================================================================================
+# Precision:
+# The precision is the ratio tp / (tp + fp) where tp is the number of true positives and 
+# fp the number of false positives. The precision is intuitively the ability of the classifier 
+# not to label as positive a sample that is negative.
+# The best value is 1 and the worst value is 0.
+
+precision = precision_score(y_test, y_pred, average='micro') # micro calculates metrics globally by counting the total true positives, false negatives and false positives.
+print("Precision:", precision)
+
+# Recall (aka Sensitivity):
+# Recall is the ratio of the correctly +ve labeled by our program to all who have atrial fibrillation in reality.
+# Recall answers the following question: Of all the people who have atrial fibrillation, how many of those we correctly predict?
+# Recall = TP/(TP+FN)
+recall = recall_score(y_test, y_pred, average='micro') # micro calculates metrics globally by counting the total true positives, false negatives and false positives.
+print("Recall:", recall) 
+
 
